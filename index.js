2c4a9da3b9253426b00368d5191d1f431b5b10c4
Rambhupal <rambhupalreddy369@gmail.com>
2023-08-17 18:35:34 +0530

    updated changes in javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+const number = 10;
+var num = 10;
+// var num = "20";
+console.log(num)
+console.log("hello rambhupal");
+var string = "hello rs"
+console.log(string)
+var isLoggedIn = false;
+var undefined;
+console.log(undefined)
+console.log(isLoggedIn)
---
+++ variables.js
@@ -0,0 +1,17 @@
+const Uid ="shwgghd";
+var firstName = "Ram";
+var middleName = "bhupal"
+let lastName = "k";
+console.log(lastName);
+var email = "test1234@gmail.com";
+var isLogged = false;
+lastName = "Kancharla";
+var country = "India";
+
+const data = `
+Full Name = ${firstName}${middleName} ${lastName}
+Email = ${email}
+isLogged = ${isLogged}
+Country = ${country}
+`
+console.log(data)
